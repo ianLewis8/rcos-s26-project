6e179860181d04465f43094960d3b11656fc5b6d
ianLewis8 <ianlewis319@gmail.com>
2026-02-20 16:15:19 -0500

    Adding Health system and UI for health system

--- cameras/OrbitControl/orbit_view.gd
+++ cameras/OrbitControl/orbit_view.gd
@@ -8,11 +8,11 @@ var active : bool = true : set = set_active
 @export_range(-90.0, 90.0, 0.1, "radians") var max_limit_x : float
 @export_range(0.0, 20.0, 0.01) var pan_rotation_val : float
 
-@export_group("Zoom variables")
-var zoom_val : float = 8.0
-@export var max_zoom_val : float
-@export var min_zoom_val : float
-@export var zoom_speed : float
+#@export_group("Zoom variables")
+#var zoom_val : float = 8.0
+#@export var max_zoom_val : float
+#@export var min_zoom_val : float
+#@export var zoom_speed : float
 
 @export_group("Aim variables")
 var cam_aimed : bool = false #if true, cam goes into "aim/shooter mode", above the play char shoulder
@@ -23,8 +23,8 @@ var aim_cam_pos_side : bool = true #false = left, true = right
 @export var mouse_mode_action : String = ""
 @export var aim_cam_action : String = ""
 @export var aim_cam_side_action : String = ""
-@export var cam_zoom_in_action : String = ""
-@export var cam_zoom_out_action : String = ""
+#@export var cam_zoom_in_action : String = ""
+#@export var cam_zoom_out_action : String = ""
 
 #references variables
 @onready var cam : Camera3D = %Camera3D
@@ -71,14 +71,15 @@ func _process(delta):
 	var joy_dir = Input.get_vector("pan_left", "pan_right", "pan_up", "pan_down")
 	
 	#position the cam according to her mode (default, aim (with left or right side))
-	if !cam_aimed: cam.position = Vector3(0.0, 0.0, zoom_val)
-	else: cam.position = Vector3(aim_cam_pos.x if aim_cam_pos_side else -aim_cam_pos.x, aim_cam_pos.y, zoom_val)
+	cam.position = Vector3(0.0, 0.0, 8)
+	#if !cam_aimed: cam.position = Vector3(0.0, 0.0, zoom_val)
+	#else: cam.position = Vector3(aim_cam_pos.x if aim_cam_pos_side else -aim_cam_pos.x, aim_cam_pos.y, zoom_val)
 	
 	#rotate cam
 	rotate_from_vector(joy_dir * Vector2(1.0, 0.5) * pan_rotation_val * delta)
 	
 	#handle zoom
-	zoom_handling(delta)
+	#zoom_handling(delta)
 	
 func rotate_from_vector(vector : Vector2):
 	#rotate cam by the vector's amount, and clamp the rotation between max up and max down values
@@ -88,9 +89,9 @@ func rotate_from_vector(vector : Vector2):
 	rotation.x -= vector.y
 	rotation.x = clamp(rotation.x, min_limit_x, max_limit_x)
 	
-func zoom_handling(delta : float):
+#func zoom_handling(delta : float):
 	#zoom in/out cam, and clamp zoom value between min and max zoom values
-	zoom_val += Input.get_axis(cam_zoom_in_action, cam_zoom_out_action) * zoom_speed * delta
-	zoom_val = clamp(zoom_val, min_zoom_val, max_zoom_val)
+	#zoom_val += Input.get_axis(cam_zoom_in_action, cam_zoom_out_action) * zoom_speed * delta
+	#zoom_val = clamp(zoom_val, min_zoom_val, max_zoom_val)
 	
 	
